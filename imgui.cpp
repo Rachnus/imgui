54a1a7e2e243920c5492676218a36143440457af
omar <omarcornut@gmail.com>
2019-04-10 15:24:49 +0200

    RangeSelect/MultiSelect: Fix so that shift+arrow when landing on an item that doesn't hold multi-select data doesn't trigger an unbounded range-select that ends up selecting everything until the end.

--- imgui.cpp
+++ imgui.cpp
@@ -7464,7 +7464,7 @@ static void ImGui::NavProcessItem(ImGuiWindow* window, const ImRect& nav_bb, con
         if (new_best)
         {
             result->ID = id;
-            result->SelectScopeId = g.MultiSelectScopeId;
+            result->SelectScopeId = g.NextItemMultiSelectScopeId;
             result->Window = window;
             result->RectRel = nav_bb_rel;
         }
@@ -7476,7 +7476,7 @@ static void ImGui::NavProcessItem(ImGuiWindow* window, const ImRect& nav_bb, con
                 {
                     result = &g.NavMoveResultLocalVisibleSet;
                     result->ID = id;
-                    result->SelectScopeId = g.MultiSelectScopeId;
+                    result->SelectScopeId = g.NextItemMultiSelectScopeId;
                     result->Window = window;
                     result->RectRel = nav_bb_rel;
                 }
